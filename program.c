3b9a09dea24b9adc09db934f6231b3b2322cad27
Anja709 <2610923@teslabg.edu.rs>
2026-04-09 12:15:47 +0200

    Update program.c

--- program.c
+++ program.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main(){
++{
   printf("Dobro dosli na povezivanje sa Git hubom");
   printf("Dobro dosli na povezivanje sa Git hubom");
   printf("Dobro dosli na povezivanje sa Git hubom");
